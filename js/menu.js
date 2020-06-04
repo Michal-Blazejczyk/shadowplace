ee785ca9d89e7d949b9a904bf9d25d48813debfc
Michal <budzik555@interia.pl>
2020-06-04 17:31:59 +0100

    new changes

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,24 @@
-const burger = document.querySelector(".burger");
+const menuBtn = document.querySelector('.menu-btn');
+const hamburger = document.querySelector('.menu-btn__burger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
+const navItem = document.querySelectorAll('.menu-nav__item');
+let showMenu = false;
 
-const iconBurger = document.querySelector(".fa-bars");
-const iconX = document.querySelector(".fa-times");
-const column = document.querySelector("aside");
-
-burger.addEventListener("click", function()
-{
-    iconBurger.classList.toggle("show")
-    iconX.classList.toggle("show")
-    column.classList.toggle("show")
-})
+menuBtn.addEventListener('click', toggleMenu);
+function toggleMenu() {
+    //! operator do sprawdzenia false/true
+    if(!showMenu) {
+        hamburger.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open');
+        navItem.forEach(item => item.classList.add ('open'));
+        showMenu = true;
+    } else {
+        hamburger.classList.remove('open');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
+        navItem.forEach(item => item.classList.remove ('open'));
+        showMenu = false;
+    }
+}
